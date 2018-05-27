ec5cbc54e941a583c195d10c99eca44bd6b4cda2
Bryant Vail <Bryanttv@outlook.com>
2018-05-27 16:48:49 -0700

    Person[] persons = {...};
        	Person p1 = new Person("Bryant", "Vail");
        	Person p2 = new Person("Shirley", "Vail");
        	Person p3 = new Person("John", "Vail");
        	Person p4 = new Person("Lewis", "Vail");
        	Person[] persons = {p1,p2,p3,p4};

--- Main.java
+++ Main.java
@@ -2,6 +2,14 @@
 
 public class Main{
     public static void main(String[] args){
+    	
+    	Person p1 = new Person("Bryant", "Vail");
+    	Person p2 = new Person("Shirley", "Vail");
+    	Person p3 = new Person("John", "Vail");
+    	Person p4 = new Person("Lewis", "Vail");
+    	Person[] persons = {p1,p2,p3,p4};
+    	
+    	
         //init array
         Data d = new Data();
         d.Display();
@@ -29,10 +37,19 @@ public class Main{
     }//end main
 }//end public class main
 
+/*
+ * 
+ * 	Class Main shall only have one method, 
+ * 		main()
+	main() shall 
+		create 
+			four objects of 
+				type Person and they shall be 
+				stored to an array.
 
-
-
-
+	The array should be 
+		part of the Class Main.
+ */
 
 
 
