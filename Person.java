72f3dc025e90ac86346e09e5cef53b5231751d02
Bryant Vail <Bryanttv@outlook.com>
2018-05-27 15:25:38 -0700

    init, get, set

--- Person.java
+++ Person.java
@@ -1,4 +1,39 @@
 
 public class Person {
+	private String firstName;
+	private String lastName	;
+	
+	public Person() {
+		initFirstName	();
+		initLastName	();
+		
+	}
+	
+	//Get/Set FirstName
+	public String GetFirstName() {
+		return this.firstName;
+	}
+	
+	public void SetFirstName(String first) {
+		this.firstName = first;
+	}
+	
+	//Get/Set LastName
+	public String GetLastName() {
+		return this.lastName;
+	}
+	
+	public void SetLastName(String last) {
+		this.lastName = last;
+	}
+	
+	public void initFirstName() {
+		firstName	= "";
+		
+	}
+	
+	public void initLastName() {
+		lastName = "";
+	}
 
 }
