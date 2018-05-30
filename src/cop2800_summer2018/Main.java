a30ced48683560ef6ede94b7b3394c587325c04e
Bryant Vail <Bryanttv@outlook.com>
2018-05-30 19:54:26 -0400

    ArrayList

--- src/cop2800_summer2018/Main.java
+++ src/cop2800_summer2018/Main.java
@@ -1,20 +1,35 @@
 //Bryant Vail
 package cop2800_summer2018;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// names are getting int he way
-		Character p1 = new Character("Snoopy");
-		Character p2 = new Character("Garfield");
 		
 		Character[] ary  = new Character[4];//no characters in array, declared memory space, enough space to hold 4 "Characters"
-		ary[0] = p1;
-		ary[1] = p2;
 		
-		System.out.println(p1.Name);
-		System.out.println(p2.Name);
-
+		ary[0] = new Character("Snoopy");
+		ary[1] = new Character("Garfield");
+		
+		//array list = 
+		
+		
+		System.out.println(ary[0]);
+		System.out.println(ary[1]);
+		
+		ArrayList<Character> CharacterList = new ArrayList<Character>(4);//Size defined, but the size can change, 
+		
+		CharacterList.add(new Character("Snoopy"));
+		//ALERT ALERT, ^ above method can be used dynamically when "Snoopy" is a parameter
+		CharacterList.add(new Character("Garfield"));
+		CharacterList.add(new Character("Bugs"));
+		CharacterList.set(0,new Character("broomfield"));
+		
+		for(int i = 0; i<CharacterList.size(); i++){
+			System.out.println(CharacterList.get(i).Name);
+		}//end for
 	}
 
 }
